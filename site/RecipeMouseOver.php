aefb76be5e907102dbd45b31e351984d7a51b432
mattshuff <10813933+mattshuff@users.noreply.github.com>
2019-07-25 20:56:50 +0100

    Add files via upload

---
+++ site/RecipeMouseOver.php
@@ -0,0 +1,25 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$databaseName = "recipeappdb";
+$connect = mysqli_connect($hostname, $username, $password, $databaseName);
+
+$Search = $_GET['QueryValue'];
+$sql = "SELECT * FROM `recipes` WHERE `RecipeName` = '";
+$sql .= $Search;
+$sql .= "'";
+
+
+$result = $connect->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["RecipeIngredients"]. "*";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
