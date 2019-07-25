277d244c972edbef5e7c214c73dc7c9ecda0cec0
mattshuff <mattshuff1@gmail.com>
2019-07-25 21:31:46 +0100

    Update RecipeMouseOver.php

--- RecipeMouseOver.php
+++ RecipeMouseOver.php
@@ -6,20 +6,13 @@ $databaseName = "u902155560_main";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
 $Search = $_GET['QueryValue'];
-$sql = "SELECT * FROM `recipes` WHERE `RecipeName` = '";
+$sql = "SELECT `RecipeNotes` FROM `recipes` WHERE `RecipeName` = '";
 $sql .= $Search;
 $sql .= "'";
 
-
 $result = $connect->query($sql);
+$row = $result->fetch_assoc();
+echo $row["RecipeNotes"];
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo $row["RecipeIngredients"]. "*";
-    }
-} else {
-    echo "0 results";
-}
 
 ?>
