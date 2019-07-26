b22a19c8d7f1f3f04c5a0cb8c7e6728084a41974
mattshuff1 <mattshuff1@gmail.com>
2019-07-26 14:53:37 +0100

    Update ToDoScript.js

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
     console.log("test");
 
-    document.getElementById("textinput").addEventListener("keyup",function(){
-        alert(document.getElementById("textinput").value);
+document.getElementById("textinput").addEventListener("keyup",function(event){
+    if(event.keyCode=13){
+        alert("success");
+    }
+    $.ajax({
+        url: "RecipeMouseOver.php",
+        type: 'GET',
+        data: {
+          QueryValue: String(MouseOverValue)
+        },
+
+        success: function (Data) {
+   
+        }
+      });
     }
     )
 })
