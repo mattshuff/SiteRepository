5159639f072d12443963fa2d99467f72609ca9ae
mattshuff1 <mattshuff1@gmail.com>
2019-07-26 14:43:14 +0100

    reset

--- ToDoScript.js
+++ ToDoScript.js
@@ -2,18 +2,7 @@ $(document).ready(function () {
     console.log("test");
 })
 
-$(function() {
-    $('myform').submit(function() {
-        alert("test");
-        $.ajax({
-            type: 'POST',
-            url: 'submit.php',
-            data: { username: $(this).name.value, 
-                    password: $(this).password.value }
-        });
-        return false;
-    }); 
-})
+
             
 
 
