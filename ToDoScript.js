f5e157633d5f2e47cfbb13b21b45bb4176d7d24f
mattshuff1 <mattshuff1@gmail.com>
2019-07-26 14:09:19 +0100

    Update ToDoScript.js

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,23 +1,25 @@
 $(document).ready(function () {
     console.log("test");
 
-    $("#submit").submit(function( event ) {
+})
 
-        console.log("ayaya");
 
-        var Content = document.getElementById("ToDoInput").value;
-        var x = document.getElementById("ToDoList");
+$("#submit").submit(function( event ) {
 
-        $.ajax({
-            url: "AddToDo.php",
-            type: 'GET',
-            data: {
-                QueryValue: String(Content),
-            },
-            success: function (Data) {
-                alert("success");
-            }
-        })
+    console.log("ayaya");
+
+    var Content = document.getElementById("ToDoInput").value;
+    var x = document.getElementById("ToDoList");
+
+    $.ajax({
+        url: "AddToDo.php",
+        type: 'GET',
+        data: {
+            QueryValue: String(Content),
+        },
+        success: function (Data) {
+            alert("success");
+        }
     })
 })
 
