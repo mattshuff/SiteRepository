c267988c12e66eeb3fe4b4798e1b381ce33d51b8
mattshuff <mattshuff1@gmail.com>
2019-07-26 13:25:05 +0100

    starting php work

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
     $("#submit").submit(function (event) {
         var Content = document.getElementById("ToDoInput").value;
-        console.log("hehe");
-        alert(Content);
+        var x = document.getElementById("ToDoList");
 
-    });
+        $.ajax({
+            url: "AddToDo.php",
+            type: 'GET',
+            data: {
+                QueryValue: String(Content),
+            },
+            success: function (Data) {
+
+            },
+        })
+    })
 })
-$.ajaxSetup({
-    cache: false
-});
