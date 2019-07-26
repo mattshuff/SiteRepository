fa2f06b1f106fe2af1fa8bf522fa81cd4a678f58
mattshuff <mattshuff1@gmail.com>
2019-07-26 12:41:05 +0100

    Update ToDoScript.js

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("#submit").submit(function (event) {
-        var Content = document.getElementById(ToDoInput).innerText;
+        var Content = document.getElementById("ToDoInput").innerText;
+        console.log("hehe");
         alert(Content);
 
     });
