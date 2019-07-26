587fa618a8ad857436b6c43cfca31a8141df585c
mattshuff1 <mattshuff1@gmail.com>
2019-07-26 17:00:40 +0100

    fixing php

--- RecipeApp/RecipeMouseOver.php
+++ RecipeApp/RecipeMouseOver.php
@@ -6,12 +6,13 @@ $databaseName = "u902155560_main";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
 $Search = $_GET['QueryValue'];
-$sql = "SELECT `RecipeNotes` FROM `recipes` WHERE `RecipeName` = '";
+$sql = "SELECT * FROM `recipes` WHERE `RecipeName` = '";
 $sql .= $Search;
 $sql .= "'";
 
 $result = $connect->query($sql);
 $row = $result->fetch_assoc();
+echo $row["RecipeIngredients"]."*";
 echo $row["RecipeNotes"];
 
 
