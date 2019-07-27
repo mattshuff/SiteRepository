cb7edf20180743f1aeb155db61c9fe846cf972e4
mattshuff1 <mattshuff1@gmail.com>
2019-07-27 15:30:00 +0100

    Update NewRecipe.php

--- RecipeApp/NewRecipe.php
+++ RecipeApp/NewRecipe.php
@@ -5,13 +5,10 @@ $password = "uew6UoDPmnb1";
 $databaseName = "u902155560_main";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
-//INSERT INTO `recipes`(`RecipeName`, `RecipeIngredients`, `RecipeNotes`) VALUES ([value-2],[value-3],[value-4])
-
 $RecipeName = $_GET['RecipeName'];
 $RecipeIngredients = $_GET['RecipeIngredients'];
 $RecipeMethod = $_GET['RecipeMethod'];
-
-
+echo $RecipeMethod;
 $sql = "INSERT INTO `recipes`(`RecipeName`, `RecipeIngredients`, `RecipeNotes`) VALUES ("
 $sql = $sql.$RecipeName.",".$RecipeIngredients.",".$RecipeMethod.")";
 echo $sql;
