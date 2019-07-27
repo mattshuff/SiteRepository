a0a8452fb19f91b66f95f61f039099994c2c2148
mattshuff1 <mattshuff1@gmail.com>
2019-07-27 15:53:38 +0100

    Update NewRecipe.php

--- RecipeApp/NewRecipe.php
+++ RecipeApp/NewRecipe.php
@@ -9,19 +9,18 @@ $username = "u902155560_admin";
 $password = "uew6UoDPmnb1";
 $databaseName = "u902155560_main";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
-echo $databaseName;
+
 $RecipeName = $_GET['RecipeName'];
 $RecipeIngredients = $_GET['RecipeIngredients'];
 $RecipeMethod = $_GET['RecipeMethod'];
 echo $RecipeMethod;
 $sql = "INSERT INTO `recipes`(`RecipeName`, `RecipeIngredients`, `RecipeNotes`) VALUES (".$RecipeName.",".$RecipeIngredients.",".$RecipeMethod.")";
 
-echo $sql;
 
 
-//if (mysqli_query($connect, $sql)) {
-//    echo "New record created successfully";
-//} else {
-//    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-//}
+if (mysqli_query($connect, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 ?>
