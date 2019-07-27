6381955f5a271cdb4241e34ec5df9d5b458df85a
mattshuff1 <mattshuff1@gmail.com>
2019-07-27 15:33:18 +0100

    Update NewRecipe.php

--- RecipeApp/NewRecipe.php
+++ RecipeApp/NewRecipe.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1); 
+ini_set('display_startup_errors', 1); 
+error_reporting(E_ALL);
+
 $hostname = "localhost";
 $username = "u902155560_admin";
 $password = "uew6UoDPmnb1";
