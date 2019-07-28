a36c8926f3ad4e64ef236ff44fff442b37c6beb3
mattshuff1 <mattshuff1@gmail.com>
2019-07-28 13:58:37 +0100

    Update RecipeApp.js

--- RecipeApp/RecipeApp.js
+++ RecipeApp/RecipeApp.js
@@ -92,7 +92,7 @@ $(document).ready(function () {
   var input = document.getElementById('TextInput');
   input.addEventListener('keyup', function () {
     var x = document.getElementById('RecipeList');
-    //$(x).empty();
+    $(x).empty();
 
     $.ajax({
       url: "LiveSearchQuery.php",
@@ -131,7 +131,6 @@ $(document).ready(function () {
               }
             });
           }
-
           ul.appendChild(li);
         }
       }
