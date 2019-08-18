f98b65b921d681b43b80fa4bfaaaa6a01d0b4c0f
mattshuff <mattshuff1@gmail.com>
2019-08-18 19:28:57 +0100

    Update Stocks.js

--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -7,20 +7,21 @@ jQuery.ajax({
     contentType: "application/json",
     success: function (data) {
         FetchedJSON = data;
+
+        var label = "Time Series (Daily)";
+        var timeData = FetchedJSON[label];
+
+        dates = [];
+        values = [];
+
+        console.log(timeData);
+
+        //iterate through our data, get dates and closing values
+        for (const key of Object.keys(timeData)) {
+            //this add the time to our dates
+            dates.push(key);
+            //this add the closing values to our values array
+            values.push(timeData[key]["4. close"]);
+        }
     }
 })
-var label = "Time Series (Daily)";
-var timeData = FetchedJSON[label];
-
-dates = [];
-values = [];
-
-console.log(timeData);
-
-//iterate through our data, get dates and closing values
-for (const key of Object.keys(timeData)) {
-    //this add the time to our dates
-    dates.push(key);
-    //this add the closing values to our values array
-    values.push(timeData[key]["4. close"]);
-}
