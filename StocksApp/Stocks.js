0d778adb1ef1449d8f4922a81f47d42a84ad07bd
mattshuff <mattshuff1@gmail.com>
2019-08-18 17:38:53 +0100

    testing dumping data onto page

--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -1,10 +1,14 @@
 var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=compact&symbol=0P0000KM22.L&interval=1min&apikey=DET6IF6YAHK5PGVO';
+var Data;
 
 jQuery.ajax({
     url: url,
     dataType: 'json',
     contentType: "application/json",
     success: function (data) {
-        console.log(data);
+        Data = data;
     }
-});
+})
+
+var p = document.getElementById("test");
+p.innerHTML = Data;
