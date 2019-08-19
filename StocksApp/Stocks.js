8967082b62b06994e46b77e4345486b83c495d0d
mattshuff1 <mattshuff1@gmail.com>
2019-08-19 14:33:05 +0100

    adding graph

--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -1,28 +1,59 @@
 var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=compact&symbol=0P0000KM22.L&interval=1min&apikey=DET6IF6YAHK5PGVO';
-var FetchedJSON = "";
 
 $.ajax({
     url: url,
     dataType: 'json',
     contentType: "application/json",
     success: function (data) {
-        FetchedJSON = data;
 
+        //select the daily data
         var label = "Time Series (Daily)";
-        var timeData = FetchedJSON[label];
+        var timeData = data[label];
 
+        //arrays to be logged to
         dates = [];
         values = [];
 
         console.log(timeData);
 
         //iterate through our data, get dates and closing values
-        for (const key of Object.keys(timeData)) {            
-            //this add the time to our dates
-            dates.push(key);
-            //this add the closing values to our values array
-            values.push(timeData[key]["4. close"]);
+        for (const date of Object.keys(timeData)) { 
+
+            dates.push(date);
+            values.push(timeData[date]["4. close"]);
         }
-        console.log(dates[0] + " " + values[0]);
+
+        //draw data to graph
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            title:{
+                text: "test graph"
+            },
+            axisX:{
+                valueFormatString: "DD MMM",
+                crosshair: {
+                    enabled: true,
+                    snapToDataPoint: true
+                }
+            },
+            axisY: {
+                title: "Closing Price (in USD)",
+                includeZero: false,
+                valueFormatString: "$##0.00",
+            },
+            data: [{
+                type: "area",
+                xValueFormatString: "DD MMM",
+                yValueFormatString: "$##0.00",
+                dataPoints: [
+                    { x: new Date(2016, 07, 01), y: 76.727997 },
+                    
+
+                ]
+            }]
+        });
+        chart.render();
+
+        
     }
 })
