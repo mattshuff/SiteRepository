edf188b6750c7ec0270c247fa930020a9ffa8c87
mattshuff1 <mattshuff1@gmail.com>
2019-08-21 11:27:31 +0100

    Update Stocks.js

--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -19,7 +19,7 @@ $.ajax({
         //iterate through our data, get dates and closing values
         for (const date of Object.keys(timeData)) { 
             var x = new Date(date);
-            x.toLocaleDateString();
+            x.toISOString().slice(0,10);
 
 
             console.log("doing work");
