027461223f3b754ce5aae8b77ef8cf7da75527de
mattshuff1 <mattshuff1@gmail.com>
2019-08-23 15:50:38 +0100

    Update Stocks.js

--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
             var data = [];
             data = PopulateData("TIME_SERIES_DAILY_ADJUSTED","SPY");
             console.log(data.length);
+            console.log(data);
 
             for(var x = 0; x < data.length; x+=1){
                 HistoryWrapper.append(data[x]);
