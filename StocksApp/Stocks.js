633183dad01a583d17955482c5ddd828fefb9899
mattshuff1 <mattshuff1@gmail.com>
2019-08-23 12:03:32 +0100

    Update Stocks.js

--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 
 
 function PopulateData(func,symbol) {
-    var APIurl = "https://www.alphavantage.co/query?function="+func+"_ADJUSTED&symbol="+symbol+"&apikey=DET6IF6YAHK5PGVO";
+    var APIurl = "https://www.alphavantage.co/query?function="+func+"&symbol="+symbol+"&apikey=DET6IF6YAHK5PGVO";
 console.log(APIurl);
     //ajax call to fetch the API
     $.ajax({
