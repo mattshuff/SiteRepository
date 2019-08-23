ee4415010a91e1f9ece460efc949ec82a4f27f39
mattshuff1 <mattshuff1@gmail.com>
2019-08-23 11:59:28 +0100

    Update Stocks.js

--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -2,11 +2,26 @@
 $(document).ready(function() {
 
 
-    //NEED TO REFACTOR ALL OF THIS INTO A FUNCTION SO THAT YOU IT WILL WORK FOR ANY CALL
+            var Content = document.getElementById("Content");
+            
+            var Tracker = document.createElement('div');
+            Tracker.setAttribute("ID","Tracker");
+            Content.append(Tracker);
+
+            var FiveDaysDiv = document.createElement('div');
+            FiveDaysDiv.setAttribute("ID","HistoryWrapper")
+            Tracker.append(FiveDaysDiv);
+
+            var data = PopulateData("TIME_SERIES_DAILY_ADJUSTED","SPY");
+
+            data.forEach(function(element) {
+                FiveDaysDiv.append(element);
+              });
+            })
 
 
-    //url of API call, eventually this will be constructed using the Tickers.txt
-    var APIurl = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=SPY&apikey=DET6IF6YAHK5PGVO';
+function PopulateData(func,symbol) {
+    var APIurl = "https://www.alphavantage.co/query?function="+func+"_ADJUSTED&symbol="+symbol+"&apikey=DET6IF6YAHK5PGVO";
 
     //ajax call to fetch the API
     $.ajax({
@@ -35,8 +50,7 @@ $(document).ready(function() {
                 values.push(ClosingValue);
             }
 
-            //setup for looping through
-            
+            //setup for looping through           
             var Elements = [];
 
             for (var x = 4; x >= 0; x -= 1) {
@@ -61,30 +75,5 @@ $(document).ready(function() {
 
             //correct order of array
             var ReversedElements = Elements.reverse();
-
-            var Content = document.getElementById("Content");
-
-            var TrackerDiv = document.createElement('div');
-            TrackerDiv.setAttribute("ID","Tracker");
-
-            var FiveDaysDiv = document.createElement('div');
-            FiveDaysDiv.setAttribute("ID","HistoryWrapper");
-
-            TrackerDiv.append(FiveDaysDiv);
-            Content.append(TrackerDiv);
-
-            //append to wrapper 
-            
-            
-
-            ReversedElements.forEach(function(element) {
-                FiveDaysDiv.append(element);
-              });
-              
-        }
-    })
-})
-
-function PopulateData(func,ticker) {
-
+            return ReversedElements;
 }
