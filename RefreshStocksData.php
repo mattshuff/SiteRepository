151e184e73fa4b1c92d29adea8aba2c206241e65
mattshuff <mattshuff1@gmail.com>
2019-10-06 15:04:04 +0100

    starting to write php and added a page exclusivley for testing new features

--- RefreshStocksData.php
+++ RefreshStocksData.php
@@ -1,3 +1,26 @@
 <?php
 
+$hostname = "localhost";
+$username = "u902155560_admin";
+$password = "uew6UoDPmnb1";
+$databaseName = "u902155560_main";
+$connect = mysqli_connect($hostname, $username, $password, $databaseName);
+
+$sql = "SELECT `CurrentIndex` FROM `Stocks` WHERE 1";
+$CurrentIndex = mysqli_query($connect, $sql);
+echo $CurrentIndex;
+
+$sql = "SELECT * FROM 'stocks'";
+$Records = mysqli_query($connect, $sql);
+echo $Records;
+
+
+$FiveDayData = file_get_contents("");
+$FiveMonthData = file_get_contents("");
+
+$func = "TIME_SERIES_DAILY_ADJUSTED";
+$symbol; //fetch this from the database 
+$APIurl = "https://www.alphavantage.co/query?function="+$func+"&symbol="+$symbol+"&apikey=DET6IF6YAHK5PGVO";
+
+mysqli_query($connect, $sql);
 ?>
