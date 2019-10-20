7dafd9502361cef77c9cab4be07a7de5f47f3085
mattshuff1 <mattshuff1@gmail.com>
2019-10-20 14:02:20 +0100

    Update RefreshStocksData.php

--- RefreshStocksData.php
+++ RefreshStocksData.php
@@ -7,13 +7,13 @@ $password = "uew6UoDPmnb1";
 $databaseName = "u902155560_main";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
-//fetch current index  *works*
+//fetch current index 
 $sql = "SELECT `CurrentIndex` FROM `Stocks` WHERE 1";
 $Result = mysqli_query($connect, $sql);
 $CurrentIndex = $Result->fetch_row();
 echo $CurrentIndex[0];
 
-//fetch all tracked tickers *works*
+//fetch all tracked tickers
 $sql = "SELECT `StockTicker` FROM `Stocks` WHERE 1";
 $Result = mysqli_query($connect, $sql);
 $Tickers = $Result->fetch_array(MYSQLI_NUM); //can be accessed via index
@@ -21,6 +21,7 @@ $Tickers = $Result->fetch_array(MYSQLI_NUM); //can be accessed via index
 
 //fetch daily 
 $func = "TIME_SERIES_DAILY_ADJUSTED";
-$symbol = $Records[$CurrentIndex]; //DONT THINK THIS WORKS 
+$symbol = $Tickers[$CurrentIndex]; 
+echo $symbol;
 $APIurl = "https://www.alphavantage.co/query?function=".$func."&symbol=".$symbol."&apikey=DET6IF6YAHK5PGVO";
 ?>
