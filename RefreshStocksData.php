c3e71b4df5634b4df584b7766b939cb8e0bc1271
mattshuff <mattshuff1@gmail.com>
2019-10-27 19:32:20 +0000

    working on testing page

--- RefreshStocksData.php
+++ RefreshStocksData.php
@@ -1,31 +1,32 @@
 <?php
 
-//setup connection to database *works*
+//create connection to database 
 $hostname = "sql160.main-hosting.eu";
 $username = "u902155560_admin";
 $password = "uew6UoDPmnb1";
 $databaseName = "u902155560_main";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
-//fetch current index *works*
+//fetch current index 
 $sql = "SELECT `CurrentIndex` FROM `Stocks` WHERE 1";
 $Result = mysqli_query($connect, $sql);
 $CurrentIndex = $Result->fetch_row();
 
-//fetch all tickers from DB *works*
+//fetch all tickers from DB 
 $sql = "SELECT `StockTicker` FROM `Stocks` WHERE 1";
 $Result = mysqli_query($connect, $sql);
 
-//convert DB query result to numeric array *works*
-$Tickers = $Result->fetch_all(MYSQLI_NUM); //can be accessed via index
+//convert DB query result to numeric array 
+$Tickers = $Result->fetch_all(MYSQLI_NUM);
 
-for ($x = 0; $x <= 2; $x++) {
-    echo $Tickers[$x][0];
-} 
-
-//fetch daily 
 $func = "TIME_SERIES_DAILY_ADJUSTED";
-$symbol = $Tickers[$CurrentIndex[0]]; 
-//echo $symbol;
-//$APIurl = "https://www.alphavantage.co/query?function=".$func."&symbol=".$symbol."&apikey=DET6IF6YAHK5PGVO";
-?>
+
+for ($x = 0; $x <= 1; $x++) {
+
+    //construct API call
+    $CurrentTicker = $Tickers[$CurrentIndex[0]][0];
+    $APIurl = "https://www.alphavantage.co/query?function=" . $func . "&symbol=" . $CurrentTicker . "&apikey=DET6IF6YAHK5PGVO";
+
+    //THIS NEEDS TO BE UPDATED IN THE DATABSE 
+    $CurrentIndex[0]++;
+}
--- StocksApp/Stocks.js
+++ StocksApp/Stocks.js
@@ -1,70 +1,70 @@
 //wait until the page is ready
-$(document).ready(function() {
-AddTracker("SPY");
-AddTracker("TSLA");
+$(document).ready(function () {
+    AddTracker("SPY");
+    AddTracker("TSLA");
 
 })
 
-           
-function AddTracker(symbol){
+
+function AddTracker(symbol) {
     var ContentWrapper = document.getElementById("Content");
 
     var Tracker = document.createElement("div");
-    Tracker.setAttribute("ID","Tracker");
+    Tracker.setAttribute("ID", "Tracker");
 
     var para = document.createElement("p");
     var node = document.createTextNode(symbol);
     para.appendChild(node);
     para.setAttribute("ID", "HistoryP");
     Tracker.append(para);
-    
+
     var FiveDayHistoryWrapper = document.createElement("div");
-    FiveDayHistoryWrapper.setAttribute("ID","HistoryWrapper");
+    FiveDayHistoryWrapper.setAttribute("ID", "HistoryWrapper");
 
     var SixMonthHistoryWrapper = document.createElement("div");
-    SixMonthHistoryWrapper.setAttribute("ID","HistoryWrapper");
+    SixMonthHistoryWrapper.setAttribute("ID", "HistoryWrapper");
 
     var FiveDayData;
-    FiveDayData = PopulateData("TIME_SERIES_DAILY_ADJUSTED",symbol);
+    FiveDayData = PopulateData("TIME_SERIES_DAILY_ADJUSTED", symbol);
 
     var SixMonthData;
-    SixMonthData = PopulateData("TIME_SERIES_MONTHLY",symbol);
+    SixMonthData = PopulateData("TIME_SERIES_MONTHLY", symbol);
 
 
 
-    for(var x = 0; x < FiveDayData.length; x+=1){
+    for (var x = 0; x < FiveDayData.length; x += 1) {
         FiveDayHistoryWrapper.append(FiveDayData[x]);
         SixMonthHistoryWrapper.append(SixMonthData[x]);
     }
-     
-      
-      Tracker.append(FiveDayHistoryWrapper);
-      Tracker.append(SixMonthHistoryWrapper);
-      ContentWrapper.append(Tracker);
+
+
+    Tracker.append(FiveDayHistoryWrapper);
+    Tracker.append(SixMonthHistoryWrapper);
+    ContentWrapper.append(Tracker);
 }
-      
 
 
-function PopulateData(func,symbol) {
-    var APIurl = "https://www.alphavantage.co/query?function="+func+"&symbol="+symbol+"&apikey=DET6IF6YAHK5PGVO";
-    var ReturnValue= [];
 
+function PopulateData(func, symbol) {
+    var APIurl = "https://www.alphavantage.co/query?function=" + func + "&symbol=" + symbol + "&apikey=DET6IF6YAHK5PGVO";
+    var ReturnValue = [];
+    console.log(APIurl);
     //ajax call to fetch the API
     $.ajax({
         url: APIurl,
         async: false,
         dataType: 'json',
         contentType: "application/json",
-        success: function(data) {
+        success: function (data) {
 
             //select the daily data
             var label;
 
-            if(func=="TIME_SERIES_DAILY_ADJUSTED"){
-                label = "Time Series (Daily)";           
+            if (func == "TIME_SERIES_DAILY_ADJUSTED") {
+                label = "Time Series (Daily)";
             }
-            if(func=="TIME_SERIES_MONTHLY"){
-                label = "Monthly Time Series";	
+            if (func == "TIME_SERIES_MONTHLY") {
+                label = "Monthly Time Series";
             }
             var timeData = data[label];
 
@@ -96,29 +96,25 @@ function PopulateData(func,symbol) {
 
                 //colour elements
                 if (values[x] > values[x + 1]) {
-                    para.setAttribute("style", "color:#03fc49;");          
+                    para.setAttribute("style", "color:#03fc49;");
                 } else {
-                    para.setAttribute("style", "color:red;");                   
+                    para.setAttribute("style", "color:red;");
                 }
                 para.setAttribute("ID", "HistoryP");
 
                 //push elements to array
-                Elements.push(para);           
+                Elements.push(para);
             }
 
             var para = document.createElement("p");
             var node = document.createTextNode(func);
             para.appendChild(node);
             para.setAttribute("ID", "HistoryP");
-            Elements.push(para);  
+            Elements.push(para);
 
-            var test = Elements.reverse(); 
-            Object.assign(ReturnValue, test);             
+            var test = Elements.reverse();
+            Object.assign(ReturnValue, test);
         }
-    })  
+    })
     return ReturnValue;
 }
-
-
-
-
