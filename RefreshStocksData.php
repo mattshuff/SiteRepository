dbcfd6bf9a47ff6be9e64e5b6474579276298349
mattshuff <mattshuff1@gmail.com>
2019-11-02 13:06:00 +0000

    Update RefreshStocksData.php

--- RefreshStocksData.php
+++ RefreshStocksData.php
@@ -7,26 +7,46 @@ $password = "uew6UoDPmnb1";
 $databaseName = "u902155560_main";
 $connect = mysqli_connect($hostname, $username, $password, $databaseName);
 
-//fetch current index 
-$sql = "SELECT `CurrentIndex` FROM `Stocks` WHERE 1";
-$Result = mysqli_query($connect, $sql);
-$CurrentIndex = $Result->fetch_row();
-
 //fetch all tickers from DB 
 $sql = "SELECT `StockTicker` FROM `Stocks` WHERE 1";
 $Result = mysqli_query($connect, $sql);
-
-//convert DB query result to numeric array 
 $Tickers = $Result->fetch_all(MYSQLI_NUM);
 
 $func = "TIME_SERIES_DAILY_ADJUSTED";
 
 for ($x = 0; $x <= 1; $x++) {
+    //fetch current index 
+    $sql = "SELECT `CurrentIndex` FROM `Stocks` WHERE 1";
+    $Result = mysqli_query($connect, $sql);
+    $CurrentIndex = $Result->fetch_row();
 
     //construct API call
     $CurrentTicker = $Tickers[$CurrentIndex[0]][0];
     $APIurl = "https://www.alphavantage.co/query?function=" . $func . "&symbol=" . $CurrentTicker . "&apikey=DET6IF6YAHK5PGVO";
 
-    //THIS NEEDS TO BE UPDATED IN THE DATABSE 
-    $CurrentIndex[0]++;
+    $JSONstring = file_get_contents($APIurl);
+    $JSONarray = json_decode($JSONstring, true);
+    echo $JSONarray[0][0];
+
+    //SETUP FOR NEXT LOOP!
+    //get number of records 
+    $sql = "select COUNT(*) from Stocks";
+    $Result = mysqli_query($connect, $sql);
+    $temp = $Result->fetch_row();
+    $RecordsCount = $temp[0];
+
+    //if we are not at the end of the records do this
+    if ($RecordsCount > $CurrentIndex[0]) {
+        echo "if";
+        $CurrentIndex[0]++;
+        $sql = "UPDATE `Stocks` SET `CurrentIndex`=" . (string) $CurrentIndex[0] . " WHERE 1";
+        $Result = mysqli_query($connect, $sql);
+    }
+    //if we are at the final record, go back to zero 
+    else {
+        echo "else";
+        $CurrentIndex[0] = 0;
+        $sql = "UPDATE `Stocks` SET `CurrentIndex`=" . (string) $CurrentIndex[0] . " WHERE 1";
+        $Result = mysqli_query($connect, $sql);
+    }
 }
