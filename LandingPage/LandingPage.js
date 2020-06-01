0d57189b5ea6c1cf370c970f9d837a7bb87b2a43
mattshuff <mattshuff1@gmail.com>
2020-06-01 12:14:36 +0100

    started adding light and dark mode

--- LandingPage/LandingPage.js
+++ LandingPage/LandingPage.js
@@ -1,40 +1,41 @@
 $(document).ready(function () {
+    LoadPreferences();
     //populate to do 
     $.ajax(
         "LandingPage/LoadToDo.php", {
-            success: function (data) {
+        success: function (data) {
 
-                //split data into array
-                var DataArray = data.split("*");
+            //split data into array
+            var DataArray = data.split("*");
 
-                //select empty list
-                var ul = document.getElementById("ToDoList");
+            //select empty list
+            var ul = document.getElementById("ToDoList");
 
-                //loop through data 
-                for (var x = 0; x < DataArray.length; x++) {
+            //loop through data 
+            for (var x = 0; x < DataArray.length; x++) {
 
-                    //create a list element and append to larger structure 
-                    var li = document.createElement("li");
-                    li.appendChild(document.createTextNode(DataArray[x]));
+                //create a list element and append to larger structure 
+                var li = document.createElement("li");
+                li.appendChild(document.createTextNode(DataArray[x]));
 
-                    //give double click function to remove it 
-                    li.ondblclick = function () {
-                        var HoverValue = this.innerHTML;
+                //give double click function to remove it 
+                li.ondblclick = function () {
+                    var HoverValue = this.innerHTML;
 
-                        $.ajax({
-                            url: 'LandingPage/DeleteToDo.php',
-                            type: 'GET',
-                            data: {
-                                QueryValue: String(HoverValue)
-                            },
-                        })
-                        $(this).remove();
-                    }
-
-                    ul.appendChild(li);
+                    $.ajax({
+                        url: 'LandingPage/DeleteToDo.php',
+                        type: 'GET',
+                        data: {
+                            QueryValue: String(HoverValue)
+                        },
+                    })
+                    $(this).remove();
                 }
+
+                ul.appendChild(li);
             }
-        })
+        }
+    })
 
 
     //add submit event listener to text input box
@@ -78,3 +79,34 @@ $(document).ready(function () {
         }
     })
 })
+function SwapColourMode(e) {
+    var SenderImageID = e.originalTarget.id;
+    console.log(SenderImageID);
+
+    if (SenderImageID = "SunIMG") {
+        //change to light mode
+        localStorage.ColourMode = "light"
+
+        //change button to moon so user can swap back to dark (dark is the default)
+        var Image = document.getElementById(SenderImageID);
+        console.log(Image);
+        Image.src = "/LandingPage/Assets/moon.png";
+        Image.id = "MoonIMG";
+    }
+    else if (SenderImageID = "MoonIMG") {
+        //change to dark mode 
+        localStorage.ColourMode = "dark"
+        var Image = document.getElementById(SenderImageID);
+        Image.src = "/LandingPage/Assets/sun.png";
+        Image.id = "SunIMG";
+    }
+}
+function LoadPreferences() {
+    var ColourMode = localStorage.ColourMode;
+    if (ColourMode = "light") {
+        console.log("colour mode is stored light ")
+    }
+    else {
+
+    }
+}
