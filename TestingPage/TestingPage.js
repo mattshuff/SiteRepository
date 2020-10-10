8db5b6edb38814eef891742e3e99d71f342fa254
mattshuff1 <mattshuff1@gmail.com>
2020-10-10 15:50:47 +0100

    Update TestingPage.js

--- TestingPage/TestingPage.js
+++ TestingPage/TestingPage.js
@@ -2,56 +2,12 @@ $(document).ready(function () {
 
 });
 
-function ChartTest() {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    display: false
 
-                }]
-            },
-            responsive: false,
-            maintainAspectRatio: false
-
-        }
-
-    });
-    ctx.width = "200px";
-    ctx.height = "400px";
-
-}
 
 function TestPHP() {
     $.ajax({
         type: "POST",
-        url: '../Test.php',
+        url: 'Test.py',
         data: "",
         async: false,
         success: function (data) //on recieve of reply
