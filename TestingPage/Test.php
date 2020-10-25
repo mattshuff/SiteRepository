59208e31d0470c6b294008f48d6faadfb228d4e7
mattshuff1 <mattshuff1@gmail.com>
2020-10-25 12:36:01 +0000

    database test

--- TestingPage/Test.php
+++ TestingPage/Test.php
@@ -1,1 +1,18 @@
-echo("string");
+<?php
+$hostname = "localhost";
+$username = "admin";
+$password = "Sagwala@1";
+$databaseName = "matt_SiteData";
+$connect = mysqli_connect($hostname, $username, $password, $databaseName);
+
+//INSERT INTO `ToDo` (`ToDoID`, `ToDoContent`) VALUES (NULL, 'test') 
+
+$Value = $_GET['QueryValue'];
+$sql = "INSERT INTO ToDo (`ToDoContent`) VALUES ('" .$Value ."')"; ;
+
+if (mysqli_query($connect, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+?>
